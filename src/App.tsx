63551d1deaa43cba3856b282dff64bf98cf5bfd2
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-21 14:31:27 +0100

    create environment

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="globe"></div>
+      <div
+        className="globe"
+        style={{
+          left: "100%",
+        }}
+      ></div>
+    </>
+  );
+}
+
+export default App;
