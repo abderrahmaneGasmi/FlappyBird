56c31c270718baf7c0d24bdbb10d9d10b290960a
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-21 16:01:29 +0100

    add bird

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,29 @@ interface Vars {
   canvahight: number;
   groundx: number;
 }
+interface BirdY {
+  birdY: number;
+  dir: "up" | "down";
+}
 function App() {
   const [canva, setcanva] = useState<HTMLCanvasElement | null>(null);
   const [ctx, setctx] = useState<CanvasRenderingContext2D | null>(null);
   const [loading, setLoading] = useState(true);
   const groundRef = React.useRef<HTMLImageElement>(null);
+  const skyRef = React.useRef<HTMLImageElement>(null);
+  const birdRef = React.useRef<HTMLImageElement>(null);
   const [vars, setVars] = useState({
     canvawith: 500,
     canvahight: 600,
     groundx: 0,
   });
+  const birdX = React.useRef<number>(0);
+  const birdY = React.useRef<BirdY>({
+    birdY: -5,
+    dir: "down",
+  });
   const box = React.useRef<HTMLDivElement>(null);
+  const frameCount = React.useRef<number>(0);
   useEffect(() => {
     if (box.current) {
       // check if the box is already has a canvas
@@ -38,8 +50,6 @@ function App() {
         } else {
           ctx.fillStyle = "#70c5cd";
           ctx.fillRect(0, 0, canva.width, canva.height);
-          const sky = new Image();
-          sky.src = "/sky.png";
 
           const groundwidth = 37;
           // repeat the ground image to fill the canvas width
@@ -51,9 +61,13 @@ function App() {
             );
           }
 
-          sky.onload = () => {
-            ctx.drawImage(sky, 0, 390, vars.canvawith, sky.height);
-          };
+          ctx.drawImage(
+            skyRef.current!,
+            0,
+            390,
+            vars.canvawith,
+            skyRef.current!.height
+          );
           setLoading(false);
 
           animate(ctx, canva, vars);
@@ -68,7 +82,6 @@ function App() {
       ctx.clearRect(0, 0, canva.width, canva.height);
       ctx.fillStyle = "#1dc5cd";
       ctx.fillRect(0, 0, canva.width, canva.height);
-      const sky = new Image();
       const groundwidth = 37;
       // repeat the ground image to fill the canvas width
       for (let i = 0; i < (vars.canvawith / groundwidth) * 2; i++) {
@@ -78,9 +91,46 @@ function App() {
           vars.canvahight - 112
         );
       }
-      sky.onload = () => {
-        ctx.drawImage(sky, 0, 390, vars.canvawith, sky.height);
-      };
+      ctx.drawImage(
+        skyRef.current!,
+        0,
+        390,
+        vars.canvawith,
+        skyRef.current!.height
+      );
+
+      ctx.drawImage(
+        birdRef.current!,
+        birdX.current,
+        0,
+        92,
+        64,
+        vars.canvawith / 2 - 46,
+        vars.canvahight / 2 - 32 + birdY.current.birdY,
+        92,
+        64
+      );
+
+      // Move to the next frame
+      frameCount.current++;
+      if (frameCount.current % 2 === 0) {
+        birdY.current.birdY += birdY.current.dir === "down" ? 1 : -1;
+        if (birdY.current.birdY > 10) {
+          birdY.current.dir = "up";
+        }
+        if (birdY.current.birdY < -10) {
+          birdY.current.dir = "down";
+        }
+      }
+      // Move to the next frame every 5 frames
+      if (frameCount.current % 7 === 0) {
+        birdX.current += 92;
+
+        // If the last frame is reached, reset to the first frame
+        if (birdX.current >= birdRef.current!.width) {
+          birdX.current = 0;
+        }
+      }
       vars.groundx -= 2;
       if (vars.groundx < -vars.canvawith + 17) {
         vars.groundx = 0;
@@ -113,6 +163,8 @@ function App() {
         }}
       ></div>
       <img ref={groundRef} src="ground.png" style={{ display: "none" }} />
+      <img ref={skyRef} src="sky.png" style={{ display: "none" }} />
+      <img ref={birdRef} src="bird.png" style={{ display: "none" }} />
     </main>
   );
 }
