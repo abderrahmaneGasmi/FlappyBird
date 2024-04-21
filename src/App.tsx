c2ca556e4f596de987e03825598bf9cb84a61a62
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-21 17:47:54 +0100

    add the pipes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ interface Vars {
   canvawith: number;
   canvahight: number;
   groundx: number;
+  Gap: number;
 }
 interface BirdY {
   birdY: number;
   dir: "up" | "down";
+}
+interface Pipe {
+  topx: number;
+  topy: number;
+  bottomx: number;
+  bottomy: number;
+  toph: number;
+  bottomh: number;
 }
 function App() {
   const [canva, setcanva] = useState<HTMLCanvasElement | null>(null);
@@ -16,16 +25,45 @@ function App() {
   const groundRef = React.useRef<HTMLImageElement>(null);
   const skyRef = React.useRef<HTMLImageElement>(null);
   const birdRef = React.useRef<HTMLImageElement>(null);
+  const pipeUpRef = React.useRef<HTMLImageElement>(null);
+  const pipeDownRef = React.useRef<HTMLImageElement>(null);
   const [vars, setVars] = useState({
     canvawith: 500,
     canvahight: 600,
     groundx: 0,
+    Gap: 250,
   });
   const birdX = React.useRef<number>(0);
   const birdY = React.useRef<BirdY>({
     birdY: -5,
     dir: "down",
   });
+  const [pipes, setPipes] = useState<Array<Pipe>>([
+    {
+      topx: vars.Gap * 3,
+      topy: 0,
+      toph: 250,
+      bottomx: vars.Gap * 3,
+      bottomy: 350,
+      bottomh: 200,
+    },
+    {
+      topx: 4 * vars.Gap,
+      topy: 0,
+      toph: 250,
+      bottomx: 4 * vars.Gap,
+      bottomy: 350,
+      bottomh: 200,
+    },
+    {
+      topx: vars.Gap * 5,
+      topy: 0,
+      toph: 250,
+      bottomx: vars.Gap * 5,
+      bottomy: 350,
+      bottomh: 200,
+    },
+  ]);
   const box = React.useRef<HTMLDivElement>(null);
   const frameCount = React.useRef<number>(0);
   useEffect(() => {
@@ -70,27 +108,22 @@ function App() {
           );
           setLoading(false);
 
-          animate(ctx, canva, vars);
+          animate(ctx, canva, vars, pipes);
         }
       }
     }
     function animate(
       ctx: CanvasRenderingContext2D,
       canva: HTMLCanvasElement,
-      vars: Vars
+      vars: Vars,
+      pipes: Array<Pipe>
     ) {
       ctx.clearRect(0, 0, canva.width, canva.height);
       ctx.fillStyle = "#1dc5cd";
       ctx.fillRect(0, 0, canva.width, canva.height);
       const groundwidth = 37;
       // repeat the ground image to fill the canvas width
-      for (let i = 0; i < (vars.canvawith / groundwidth) * 2; i++) {
-        ctx.drawImage(
-          groundRef.current!,
-          i * groundwidth + vars.groundx,
-          vars.canvahight - 112
-        );
-      }
+
       ctx.drawImage(
         skyRef.current!,
         0,
@@ -102,13 +135,14 @@ function App() {
       ctx.drawImage(
         birdRef.current!,
         birdX.current,
+        // 0,
         0,
-        92,
-        64,
-        vars.canvawith / 2 - 46,
-        vars.canvahight / 2 - 32 + birdY.current.birdY,
-        92,
-        64
+        69,
+        48,
+        vars.canvawith / 2 - 24,
+        vars.canvahight / 2 - 24 + birdY.current.birdY,
+        50,
+        40
       );
 
       // Move to the next frame
@@ -122,23 +156,70 @@ function App() {
           birdY.current.dir = "down";
         }
       }
-      // Move to the next frame every 5 frames
+      pipes.forEach((pipe) => {
+        pipe.topx -= 2;
+        pipe.bottomx -= 2;
+        if (pipe.topx < -50) {
+          pipe.topx = vars.canvawith + vars.Gap;
+          pipe.bottomx = vars.canvawith + vars.Gap;
+          const PipesHeight = generateRandomPairIn();
+          pipe.toph = PipesHeight[0];
+          pipe.bottomy = PipesHeight[1];
+        }
+      });
       if (frameCount.current % 7 === 0) {
-        birdX.current += 92;
+        birdX.current += 69;
 
         // If the last frame is reached, reset to the first frame
         if (birdX.current >= birdRef.current!.width) {
           birdX.current = 0;
         }
       }
+
+      // draw pipes
+      pipes.forEach((pipe) => {
+        // const PipesHeight = generateRandomPairIn(vars.canvahight - 112);
+
+        ctx.drawImage(
+          pipeDownRef.current!,
+          pipe.topx,
+          pipe.topy,
+          50,
+          pipe.toph
+        );
+        ctx.drawImage(
+          pipeUpRef.current!,
+          pipe.bottomx,
+          // vars.canvahight - 100 - pipeDownRef.current!.height,
+          pipe.bottomy,
+          50,
+          pipe.bottomh + pipeDownRef.current!.height
+        );
+      });
+      for (let i = 0; i < (vars.canvawith / groundwidth) * 2; i++) {
+        ctx.drawImage(
+          groundRef.current!,
+          i * groundwidth + vars.groundx,
+          vars.canvahight - 112
+        );
+      }
       vars.groundx -= 2;
       if (vars.groundx < -vars.canvawith + 17) {
         vars.groundx = 0;
       }
       // }
-      requestAnimationFrame(() => animate(ctx, canva, vars));
+      requestAnimationFrame(() => animate(ctx, canva, vars, pipes));
     }
-  }, [canva, ctx, vars]);
+  }, [canva, ctx, vars, pipes]);
+
+  function generateRandomPairIn(): [number, number] {
+    const number = generateNumber();
+
+    return [number - 150, number];
+  }
+  function generateNumber(min: number = 230, max: number = 380): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
   return (
     <main className="main">
       <div className="globe"></div>
@@ -164,7 +245,9 @@ function App() {
       ></div>
       <img ref={groundRef} src="ground.png" style={{ display: "none" }} />
       <img ref={skyRef} src="sky.png" style={{ display: "none" }} />
-      <img ref={birdRef} src="bird.png" style={{ display: "none" }} />
+      <img ref={birdRef} src="bird2.png" style={{ display: "none" }} />
+      <img ref={pipeUpRef} src="PipeUp.png" style={{ display: "none" }} />
+      <img ref={pipeDownRef} src="PipeDown.png" style={{ display: "none" }} />
     </main>
   );
 }
